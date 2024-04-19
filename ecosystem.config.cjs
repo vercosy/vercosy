8ee86f1ec95ef87cf23f8d7f8cb4e6ccc9d28fb7
Zack <lorenx@163.com>
2024-04-19 20:00:26 +0800

    feat: update pm2

--- ecosystem.config.cjs
+++ ecosystem.config.cjs
@@ -1,6 +1,11 @@
 module.exports = {
   name: 'vercosy',
   script: '.next/standalone/server.js',
-  instances: 1,
-  interpreter: "~/.bun/bin/bun", // Path to the Bun interpreter
+  instances : "max",
+  exec_mode : "cluster",
+  interpreter: "bun", // Path to the Bun interpreter
+  env: {
+    HOSTNAME: '0.0.0.0',
+    PORT: 3000,
+  },
 }
